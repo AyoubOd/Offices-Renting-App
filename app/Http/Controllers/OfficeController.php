556cb0d7de43295951ffeac58695b0cf75061960
AyoubOd <ayouboudada31@gmail.com>
2022-06-20 16:22:40 +0100

    another commit

--- app/Http/Controllers/OfficeController.php
+++ app/Http/Controllers/OfficeController.php
@@ -15,11 +15,7 @@ class OfficeController extends Controller
             ->where('approval_status', Office::APPROVAL_APPROVED)
             ->where('hidden', false)
             ->when(request('host_id'), fn ($query) => $query->where('user_id', request('host_id'))) // the one who owns the office
-            ->when(request('user_id'), fn ($query) => $query->whereHas(
-                'reservations',
-                fn ($query) =>
-                $query->where("user_id", request('user_id'))
-            )) // all the offices reserved by this user
+            ->when(request('user_id'), fn ($query) => $query->whereRelation('reservations', 'user_id', '=', request('user_id'))) // all the offices reserved by this user
             ->latest('id')
             ->paginate(20);
 
